9d696b786d30c79047f4b775660d19cc14530857
DmitryEverty <evertymusic@gmail.com>
2019-07-04 13:55:38 +0300

    figuring out things

--- engine.py
+++ engine.py
@@ -1,7 +1,20 @@
 import PageTemplate.MainWindow_support as window
+import glob
+import os
 
 
 def pprint():
     window.w.Label.configure(text='Waiting')
     a = window.w.InputField.get('0.0', 'end')
     print(a.strip())
+
+
+def saveToTxtFile(*args):
+    userProfileFolder = os.getenv('USERPROFILE')
+    targetTextFile = userProfileFolder + "\\Desktop\\file.txt"
+    inputFieldText = window.w.InputField.get('0.0', 'end')
+    print('Saved')
+    with open(targetTextFile, 'a') as file:
+        file.write(inputFieldText.strip() + '\n')
+    window.w.InputField.delete('0.0', 'end')
+# event=None
